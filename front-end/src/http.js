e1575bd483bc1ba1b7321fab64382179a20c6ac8
Dawn Inator <126039858+Dawn-Inator@users.noreply.github.com>
2024-04-16 17:40:16 +0800

    Update http.js

--- front-end/src/http.js
+++ front-end/src/http.js
@@ -6,7 +6,7 @@ import store from './store'
 
 // 基础配置
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'https://www.madblog.ga:5000';
+  axios.defaults.baseURL = 'https://api.com:5000';
 } else {
   axios.defaults.baseURL = 'http://127.0.0.1:5000';
 }
